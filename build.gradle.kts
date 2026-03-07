a6d5b3fd8ebb43b3cbcd8506190651b523162c20
MarisUK <marss88s@gmail.com>
2026-03-07 17:03:13 +0200

    M-Core Supreme Sync (Android Professional Architecture)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 buildscript {
+    ext.kotlin_version = "1.5.31"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:")
     }
 }
 
@@ -14,4 +15,8 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
